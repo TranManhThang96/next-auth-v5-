0ad25ae7115d5e48daae96b952abf93e7c26c371
thangtm <thang.tran@uptempo-global.com>
2024-02-19 13:56:03 +0700

    card wrapper

---
+++ components/auth/back-button.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+const BackButton = ({ href, label }: { label: string; href: string }) => {
+  return (
+    <Button variant="link" className="w-full font-normal" size="sm" asChild>
+      <Link href={href}>{label}</Link>
+    </Button>
+  );
+};
+
+export default BackButton;
