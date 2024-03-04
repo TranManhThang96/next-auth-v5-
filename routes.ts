44101e01ef5891e2536c2d32aae33630ca31a7db
thangtm <thang.tran@uptempo-global.com>
2024-03-04 17:44:46 +0700

    email verification

--- routes.ts
+++ routes.ts
@@ -3,7 +3,7 @@
  * @type {string[]}
  */
 
-export const publicRoutes = ['/'];
+export const publicRoutes = ['/', '/auth/new-verification'];
 
 export const authRoutes = ['/auth/login', '/auth/register', '/auth/error'];
 
