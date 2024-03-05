a24c36d9d701c2a9eff1b78190ae3f36c102faae
thangtm <thang.tran@uptempo-global.com>
2024-03-05 15:34:36 +0700

    admin page

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,7 +1,57 @@
+'use client';
+
+import { admin } from '@/actions/admin';
+import { RoleGate } from '@/components/auth/role-gate';
+import { FormSuccess } from '@/components/form-success';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { useCurrentRole } from '@/hooks/use-current-role';
+import { UserRole } from '@prisma/client';
 import React from 'react';
+import { toast } from 'sonner';
 
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  const role = useCurrentRole();
+  const onApiRouteClick = () => {
+    fetch('/api/admin').then((response) => {
+      if (response.ok) {
+        toast.success('Allowed API Route');
+      } else {
+        toast.error('FORBIDDEN');
+      }
+    });
+  };
+
+  const onServerRouteClick = () => {
+    admin().then((data) => {
+      if (data.success) {
+        toast.success(data.success);
+      } else {
+        toast.error(data?.error || '');
+      }
+    });
+  };
+
+  return (
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-center text-2xl font-semibold">🔒 Admin</p>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <FormSuccess message="You are allowed to see this content" />
+        </RoleGate>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium">Admin-only API Route</p>
+          <Button onClick={onApiRouteClick}>Click to test</Button>
+        </div>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium">Admin-only Server Action</p>
+          <Button onClick={onServerRouteClick}>Click to test</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AdminPage;
